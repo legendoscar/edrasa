9eeed9ebbfdf0055b3b1d649998e000f2aeba98f
Claret Nnamocha <devclareo@gmail.com>
2021-06-05 14:33:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,6 @@
  */
 
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
-die(ENVIRONMENT);
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
