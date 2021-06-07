b5b01425c760eb400a4b4a7231982162ecde7d98
Claret Nnamocha <devclareo@gmail.com>
2021-06-07 17:36:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 
-
+die('ff');
 $env = $_SERVER['SERVER_NAME'] === 'localhost' ? 'development' : 'production';
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 /*
