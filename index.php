28615fe2aa5fba9df74d5dc0256c27c1d6dacffd
Claret Nnamocha <devclareo@gmail.com>
2021-06-07 18:04:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 
-
+die('here');
 $env = in_array($_SERVER['SERVER_NAME'], ['localhost', 'losinsms-dev.herokuapp.com']) ? 'development' : 'production';
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : $env);
 /*
