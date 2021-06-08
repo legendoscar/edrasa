50f19357a5d37fa74bd942877c4a594ced84816c
Claret Nnamocha <devclareo@gmail.com>
2021-06-08 09:14:00 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,8 +73,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $active_group = $_SERVER['SERVER_NAME'] === 'localhost' ? 'default' : 'production';
 
-$host = ($active_group === 'production') && str_ends_with($_SERVER['SERVER_NAME'], 'losinsms.com') ? 'localhost' : 'losinsms.com';
-
 $query_builder = true;
 
 
